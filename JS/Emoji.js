6d44067bb3a8cde3905419fcd5bd9c90a22235a6
Stanko998 <49946790+Stanko998@users.noreply.github.com>
2023-07-21 09:27:30 +0200

    Emoji koa gotov

--- JS/Emoji.js
+++ JS/Emoji.js
@@ -1,5 +1,16 @@
 import data from "../json/champions.json" assert { type: "json" };
 
+let holder = document.getElementsByClassName("holder")[0];
+
 for (let i = 0; i < data.length; i++) {
-  console.log(data[i].id);
+  let item = document.createElement("div");
+  item.style.backgroundImage = "url('" + data[i].icon + "')";
+
+  let name = document.createElement("p");
+  name.textContent = data[i].id;
+
+  item.appendChild(name);
+  holder.appendChild(item);
 }
+
+console.log(data[1]);
